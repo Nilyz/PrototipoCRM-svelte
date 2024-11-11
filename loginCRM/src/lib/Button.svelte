abd2217ff77250ff1bf4eb77fff0b7466d743cfa
Yilin <yilin.zh135@gmail.com>
2024-11-11 21:20:42 +0100

    Add button component

---
+++ loginCRM/src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+
+export let btnText = "Iniciar";
+export let btnWidth = "50%";
+export let btnHeight = "60px";
+export let onClick = () => {};
+
+</script>
+
+<button on:click={onClick} style="width: {btnWidth}; height: {btnHeight}; ">
+    {btnText}
+</button>
+
+<style>
+
+</style>
---
+++ loginCRM/src/screens/Login.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Button from "../lib/Button.svelte";
+    
+</script>
+<main>
+    <Button/>
+</main>
+
+<style>
+    body{
+        height: 100vh;
+        width: 100vw;
+        background-color: #917161;
+    }
+</style>
